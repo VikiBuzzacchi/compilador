125575810aafd6b18863ee31dfbea6876719d107
TrinidadSA <aguirretrini97@gmail.com>
2026-09-18 19:50:38 -0300

    Modificaciones para correr el main

--- main.c
+++ main.c
@@ -5,16 +5,25 @@ extern int yyparse();
 extern struct node *root;
 extern int lexical_errors;
 extern int syntax_errors;
+extern FILE *yyin;
 
-int main() {
+int main(int argc, char **argv) {
+     if (argc > 1) {
+        yyin = fopen(argv[1], "r");
+        if (!yyin) {
+            perror("No se pudo abrir el archivo");
+            return 1;
+        }
+    }
+
     int ok = yyparse();
 
     if (ok == 0 && lexical_errors == 0 && syntax_errors == 0) {
         printf("\n--- Análisis sintáctico exitoso ---\n");
-        printf("\n--- Generando Código Pseudo-Assembly ---\n");
-        gen_code(root);
-        printf("\n--- Evaluando AST (Intérprete) ---\n");
-        eval(root);
+        //printf("\n--- Generando Código Pseudo-Assembly ---\n");
+        //gen_code(root);
+        //printf("\n--- Evaluando AST (Intérprete) ---\n");
+        //eval(root);
     } else {
         printf("\nFallo en el análisis sintáctico.\n");
 
